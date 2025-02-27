9a313748034219695cc5926486f19bcd59746730
Julius Koenig <julius@koenig-site.de>
2025-02-27 19:04:58 +0100

    Refactor project for FastAPI server implementation
    Replaced the script-based structure with a FastAPI server, updating configuration, settings, and dependencies to align with modern web framework usage. Removed unused functionalities, revised settings management, and streamlined Nextcloud integration for dynamic and cached contact fetching. Updated Python version and cleaned up extraneous files and dependencies.

--- src/micro_sip_nextcloud_bridge/__main__.py
+++ src/micro_sip_nextcloud_bridge/__main__.py
@@ -1,160 +1,150 @@
-import configparser
-import os
-import re
-import codecs
+import tempfile
+import time
 from pathlib import Path
 
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import requests
 from requests.auth import HTTPBasicAuth
-from vobject import readOne
-from vobject.vcard import VCard3_0
+from vobject.base import readOne
+from wiederverwendbar.logger import LoggerSingleton
+from wiederverwendbar.uvicorn import UvicornServer
 
-from micro_sip_nextcloud_bridge import __title__, __version__, __author__, __author_email__
-from micro_sip_nextcloud_bridge.logger import logger
-from micro_sip_nextcloud_bridge.settings import settings
+from micro_sip_nextcloud_bridge import __name__ as __module_name__, __title__, __description__, __version__, __author__, __author_email__
+from micro_sip_nextcloud_bridge.settings import Settings
 
-settings()
+FILES_PATH = Path(tempfile.gettempdir()) / __module_name__
 
-MICRO_SIP_CONFIG_PATH = Path(os.environ["APPDATA"]) / "MicroSIP"
+settings = Settings()
 
+logger = LoggerSingleton(name=__module_name__, settings=settings, init=True)
 
-def set_micro_sip_config():
-    logger().info("Setting MicroSIP config ...")
-
-    MICRO_SIP_CONFIG_PATH.mkdir(parents=True, exist_ok=True)
-
-    # read src config
-    with codecs.open(settings().micro_sip_src_config_path, "r", encoding="utf-16le") as f:
-        micro_sip_src_config_content = f.read()
-
-    # remove first symbol in not '['
-    if micro_sip_src_config_content[0] != "[":
-        micro_sip_src_config_content = micro_sip_src_config_content[1:]
-
-    # replace '%USERNAME% with environment variable
-    micro_sip_src_config_content = micro_sip_src_config_content.replace('%USERNAME%', os.environ["USERNAME"])
-
-    # write config
-    with codecs.open(MICRO_SIP_CONFIG_PATH / settings().micro_sip_src_config_path.name, "w", encoding="utf-16") as f:
-        f.write(micro_sip_src_config_content)
-
-    # append accounts to config file
-    for micro_sip_src_account_file in settings().micro_sip_src_accounts_path:
-        # read account file
-        with codecs.open(micro_sip_src_account_file, "r", encoding="utf-16") as f:
-            micro_sip_src_account_file_content = f.read()
-
-        # remove first symbol in not '['
-        if micro_sip_src_account_file_content[0] != "[":
-            micro_sip_src_account_file_content = micro_sip_src_account_file_content[1:]
-
-        # write to config
-        with codecs.open(MICRO_SIP_CONFIG_PATH / settings().micro_sip_src_config_path.name, "a", encoding="utf-16") as f:
-            f.write(micro_sip_src_account_file_content)
-
-    logger().info("Setting MicroSIP config ... done")
+app = FastAPI(
+    title=__title__,
+    description=__description__,
+    version=__version__,
+    contact={
+        "name": __author__,
+        "email": __author_email__,
+    }
+)
 
 
-def get_nextcloud_address_books() -> list[VCard3_0]:
-    logger().info("Getting Nextcloud Address books ...")
-
-    cards = []
-    for nextcloud_address_book in settings().nextcloud_address_books:
-        logger().info(f"Getting address book '{nextcloud_address_book.url}' ...")
-        try:
-            # retrieves CardDAV data
-            response = requests.get(f"{nextcloud_address_book.url}/?export", auth=HTTPBasicAuth(nextcloud_address_book.user, nextcloud_address_book.password))
-            nextcloud_address_book_data = str(response.content.decode("utf-8"))
-
-            # Turns the retrieved data into VCard objects
-            _l = str(nextcloud_address_book_data).replace('\\r\\n', '\n').split('END:VCARD')
-            _l.pop(-1)
-            _cards = []
-            for i in _l:
-                v = readOne(i + 'END:VCARD\n')
-                try:
-                    _o = v.org.value
-                except:
-                    v.add('org').value = ['']
-                _cards.append(v)
-            cards.extend(_cards)
-            logger().info(f"Got {len(_cards)} contacts from address book '{nextcloud_address_book.url}'.")
-        except Exception as e:
-            logger().error(f"Error while getting address book '{nextcloud_address_book.url}':\n{e}")
-    logger().info("Getting Nextcloud Address books ... done")
-    return cards
-
-
-def get_country_code() -> str:
-    """
-    Tries to find country code info in MicroSIP config file and returns it.
-    """
-
-    micro_sip_config = configparser.ConfigParser()
-    micro_sip_config.read_file(codecs.open(MICRO_SIP_CONFIG_PATH / settings().micro_sip_src_config_path.name, "r", "utf16"))
-
-    p = re.compile('(\+[0-9]{1,3})')
-    for section in micro_sip_config.sections():
-        if micro_sip_config.has_option(section, "dialPlan"):
-            m = p.search(micro_sip_config.get(section, "dialPlan"))
-            if m:
-                return m.group(1)
-    return "+49"
-
-
-def nice_number(phone_number) -> str:
+def nice_number(phone_number: str, country_code: str) -> str:
     """
     Strips space, -, ( and ) from the given string, replaces counrty cody with 0 and returns the string
     """
 
-    nice_phone_number = phone_number.replace(get_country_code(), "0").replace(" ", "").replace("-", "").replace("(", "").replace(")", "").strip()
+    nice_phone_number = phone_number
+    nice_phone_number = nice_phone_number.replace(" ", "")
+    nice_phone_number = nice_phone_number.replace("-", "")
+    nice_phone_number = nice_phone_number.replace("(", "")
+    nice_phone_number = nice_phone_number.replace(")", "")
+    if nice_phone_number.startswith("0"):
+        nice_phone_number = country_code + nice_phone_number[1:]
     return nice_phone_number
 
 
-def export_to_xml(cards):
-    """
-    Exports the given list into an XML file
-    """
+last_contacts_update: float = 0
 
-    logger().info("Writing Contacts.xml ...")
 
-    f = open(MICRO_SIP_CONFIG_PATH / "Contacts.xml", "w", encoding="utf8")
-    f.write(u'\ufeff')
-    f.write('<?xml version="1.0"?>\n')
-    f.write('<contacts>\n')
-    for card in cards:
-        try:
-            if card.org.value[0] != '':
-                _org = '{}, '.format(card.org.value[0])
-            else:
-                _org = ''
-            for tel in card.tel_list:
-                if tel.type_param == 'HOME':
-                    f.write('<contact number="{}"  name="{} ({}Home)" presence="0" directory="0" ></contact>\n'.format(nice_number(tel.value), card.fn.value, _org))
-                elif tel.type_param == 'WORK':
-                    f.write('<contact number="{}" name="{} ({}Work)" presence="0" directory="0" ></contact>\n'.format(nice_number(tel.value), card.fn.value, _org))
-                elif tel.type_param == 'CELL':
-                    f.write('<contact number="{}"  name="{} ({}Mobile)" presence="0" directory="0" ></contact>\n'.format(nice_number(tel.value), card.fn.value, _org))
-                else:
-                    f.write('<contact number="{}"  name="{} ({}Voice)" presence="0" directory="0" ></contact>\n'.format(nice_number(tel.value), card.fn.value, _org))
-        except:
-            continue
-    f.write('</contacts>\n')
-    f.close()
+@app.get("/", response_class=FileResponse, summary="Returns contacts as xml")
+def contacts() -> FileResponse:
+    global last_contacts_update
 
-    logger().info("Writing Contacts.xml ... done")
+    logger.info("Getting contacts ...")
+
+    FILES_PATH.mkdir(parents=True, exist_ok=True)
+
+    xml_file_path = FILES_PATH / "contacts.xml"
+
+    if settings.use_cached_contacts:
+        cache_timeout = last_contacts_update + settings.contacts_update_interval - time.time()
+        if cache_timeout < 0:
+            cache_timeout = 0
+    else:
+        cache_timeout = 0
+
+    if cache_timeout == 0:
+        if xml_file_path.is_file():
+            xml_file_path.unlink()
+
+    if not xml_file_path.is_file():
+        logger.info("Getting Nextcloud Address books ...")
+
+        xml_file = open(xml_file_path, "w", encoding="utf8")
+        xml_file.write(u"\ufeff")
+        xml_file.write("<?xml version=\"1.0\"?>\n")
+        xml_file.write("<contacts>\n")
+
+        for address_book in settings.address_books:
+            logger.info(f"Getting address book \"{address_book.url}\" ...")
+            try:
+                # retrieves CardDAV data
+                response = requests.get(f"{address_book.url}/?export", auth=HTTPBasicAuth(address_book.user, address_book.password))
+                address_book_data = str(response.content.decode("utf-8"))
+
+                # Turns the retrieved data into VCard objects
+                for i in str(address_book_data).replace("\\r\\n", "\n").split("END:VCARD")[:-1]:
+                    card = readOne(i + "END:VCARD\n")
+                    try:
+                        _o = card.org.value
+                    except:
+                        card.add("org").value = [""]
+                    try:
+                        if card.org.value[0] != "":
+                            _org = "{}, ".format(card.org.value[0])
+                        else:
+                            _org = ""
+                        for tel in getattr(card, "tel_list", []):
+                            if tel.type_param == "HOME":
+                                xml_file.write("<contact number=\"{}\"  name=\"{} ({}Home)\" presence=\"0\" directory=\"0\" ></contact>\n".format(
+                                    nice_number(phone_number=tel.value, country_code=address_book.country_code),
+                                    card.fn.value,
+                                    _org))
+                            elif tel.type_param == "WORK":
+                                xml_file.write("<contact number=\"{}\" name=\"{} ({}Work)\" presence=\"0\" directory=\"0\" ></contact>\n".format(
+                                    nice_number(phone_number=tel.value, country_code=address_book.country_code),
+                                    card.fn.value,
+                                    _org))
+                            elif tel.type_param == "CELL":
+                                xml_file.write(
+                                    "<contact number=\"{}\"  name=\"{} ({}Mobile)\" presence=\"0\" directory=\"0\" ></contact>\n".format(
+                                        nice_number(phone_number=tel.value, country_code=address_book.country_code),
+                                        card.fn.value,
+                                        _org))
+                            else:
+                                xml_file.write(
+                                    "<contact number=\"{}\"  name=\"{} ({}Voice)\" presence=\"0\" directory=\"0\" ></contact>\n".format(
+                                        nice_number(phone_number=tel.value, country_code=address_book.country_code),
+                                        card.fn.value,
+                                        _org))
+                    except Exception as e:
+                        logger.error(f"Error while processing contact \"{card.fn.value}\" from address book \"{address_book.url}\":\n{e}")
+            except Exception as e:
+                logger.error(f"Error while getting address book \"{address_book.url}\":\n{e}")
+        logger.info("Getting Nextcloud Address books ... done")
+
+        xml_file.write("</contacts>\n")
+        xml_file.close()
+        last_contacts_update = time.time()
+    else:
+        logger.info("Using cached contacts")
+
+    return FileResponse(
+        path=xml_file_path,
+        media_type="application/xml",
+
+    )
 
 
 def main():
-    logger().info(f"{__title__}")
-    logger().info(f"Version: {__version__}")
-    logger().info(f"Author: {__author__} ({__author_email__})")
+    logger.info(f"{__title__}")
+    logger.info(f"Version: {__version__}")
+    logger.info(f"Author: {__author__} ({__author_email__})")
 
-    set_micro_sip_config()
-
-    cards = get_nextcloud_address_books()
-    export_to_xml(cards)
+    UvicornServer(app=app, settings=settings)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/micro_sip_nextcloud_bridge/settings.py
+++ src/micro_sip_nextcloud_bridge/settings.py
@@ -1,23 +1,15 @@
-import os
-import sys
-from pathlib import Path
-
-from pydantic import Field, BaseModel, FilePath, DirectoryPath, ValidationError
-from wiederverwendbar.singleton import Singleton
-from wiederverwendbar.pydantic import ModelSingleton, FileConfig
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings
 from wiederverwendbar.logger import LoggerSettings
-
-SETTINGS_FILE_NAME: str = "settings.json"
-SETTINGS_LOOKUP_PATHS: list[Path] = [
-    Path("."),
-    Path("/usr/local/etc/nextcloud-micos-export"),
-    Path("/usr/local/opt/nextcloud-micos-export"),
-    Path("/etc/nextcloud-micos-export"),
-    Path("/opt/nextcloud-micos-export")
-]
+from wiederverwendbar.uvicorn import UvicornServerSettings
 
 
-class Settings(FileConfig, LoggerSettings, metaclass=ModelSingleton):
+class Settings(BaseSettings, UvicornServerSettings, LoggerSettings):
+    model_config = {
+        "env_prefix": "MICRO_SIP_NEXTCLOUD_BRIDGE_",
+        "case_sensitive": False
+    }
+
     class NextcloudAddressBook(BaseModel):
         url: str = Field(default=...,
                          title="Nextcloud URL",
@@ -28,40 +20,16 @@ class Settings(FileConfig, LoggerSettings, metaclass=ModelSingleton):
         password: str = Field(default=...,
                               title="Nextcloud Password",
                               description="Password for the Nextcloud server.")
+        country_code: str = Field(default="+49",
+                                  title="Country Code",
+                                  description="Country code for the Nextcloud server.")
 
-    micro_sip_path: FilePath = Field(default=...,
-                                     title="MicroSIP Path",
-                                     description="Path to the MicroSIP installation.")
-    micro_sip_src_config_path: FilePath = Field(default=...,
-                                                title="MicroSIP Source Config Path",
-                                                description="Path to the MicroSIP source config.")
-    micro_sip_src_accounts_path: list[FilePath] = Field(default_factory=list,
-                                                        title="MicroSIP Source Accounts Path",
-                                                        description="List of paths to the MicroSIP source accounts.")
-    nextcloud_address_books: list[NextcloudAddressBook] = Field(default_factory=list,
-                                                                title="Nextcloud Address Books",
-                                                                description="List of Nextcloud address books.")
-
-
-def settings() -> Settings:
-    try:
-        return Singleton.get_by_type(Settings)
-    except RuntimeError:
-        try:
-            s = None
-            for path in SETTINGS_LOOKUP_PATHS:
-                file_path = path / SETTINGS_FILE_NAME
-                if not file_path.is_file():
-                    continue
-                s = Settings(file_path=file_path, file_must_exist=True, init=True)
-                break
-            if s is None:
-                raise FileNotFoundError(f"No '{SETTINGS_FILE_NAME}' file found. Possible paths:\n"
-                                        f"{f'{chr(10)}'.join([str(path.absolute() / SETTINGS_FILE_NAME) for path in SETTINGS_LOOKUP_PATHS])}")
-            return s
-        except FileNotFoundError as e:
-            print(e)
-            sys.exit(1)
-        except ValidationError as e:
-            print(e)
-            sys.exit(1)
+    address_books: list[NextcloudAddressBook] = Field(default_factory=list,
+                                                      title="Nextcloud Address Books",
+                                                      description="List of Nextcloud address books.")
+    use_cached_contacts: bool = Field(default=False,
+                                      title="Use Cached Contacts",
+                                      description="Use cached contacts instead of fetching them from the Nextcloud server.")
+    contacts_update_interval: int = Field(default=900,
+                                          title="Contacts Update Interval in Seconds",
+                                          description="Interval in seconds for updating contacts.")
